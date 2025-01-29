972021fa5c32dd9e67cb0f595997baca67799746
inoue <emocedigital@gmail.com>
2025-01-29 12:05:56 +0900

    init

---
+++ webpack.config.js
@@ -0,0 +1,97 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const path = require('path');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const glob = require('glob');
+
+// HTMLファイルを動的に取得し、必要なバンドルを割り当てる
+const htmlPlugins = glob.sync('./src/**/*.html').map((file) => {
+  const filename = path.relative('./src', file);
+
+  let outputFilename;
+  let chunks;
+
+  if (filename === 'lp1/index.html') {
+    outputFilename = 'lp1/index.html';
+    chunks = ['lp1'];
+  }
+
+  return new HtmlWebpackPlugin({
+    filename: outputFilename,
+    template: file,
+    inject: 'body',
+    chunks,
+    scriptLoading: 'module',
+    minify: false,
+  });
+});
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    lp1: ['./src/js/utils.js', './src/js/lp1.js', './src/css/lp1.css'],
+  },
+  output: {
+    filename: (pathData) => {
+      if (pathData.chunk.name === 'lp1') {
+        return 'lp1/js/[name].bundle.js';
+      }
+    },
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, 'src'),
+    },
+    hot: true,
+    open: true,
+    watchFiles: {
+      paths: ['src/**/*.html', 'src/**/*.css', 'src/**/*.js'],
+      options: {
+        usePolling: true,
+      },
+    },
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      },
+      {
+        test: /\.(png|jpg|gif|webp|svg)$/i,
+        type: 'asset/resource',
+        generator: {
+          filename: 'images/[name][ext]',
+        },
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.js'],
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: ({ chunk }) => {
+        if (chunk.name === 'lp1') {
+          return 'lp1/css/[name].css';
+        }
+      },
+    }),
+    ...htmlPlugins,
+    new CopyWebpackPlugin({
+      patterns: [{ from: 'src/images', to: 'images' }],
+    }),
+  ],
+};
